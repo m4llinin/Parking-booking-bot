a80ed6e545d6527bfa64f3a0b6f02ad6977e4d95
Denis <ddenis@mail.ru>
2023-11-25 22:23:39 +0300

    1.2

--- handlers/my_books/Inline_my_books.py
+++ handlers/my_books/Inline_my_books.py
@@ -1,2 +1,11 @@
-#Здесь должно быть обращение к БД и получение из нее данных и подсчет количества записей count
-count=22
+#Требует доработки
+
+from aiogram.types import CallbackQuery
+
+async def inline_my_books(callback:CallbackQuery):
+    booking = ""
+    for i in callback.data:
+        if i.isdigit():
+            booking+=i
+    booking=int(booking)
+
