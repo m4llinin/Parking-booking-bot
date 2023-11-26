b45279c2fa5e57416f2d7b027ed9ff01f028edce
kostenbl4 <sasha.kostenko.2005@mail.ru>
2023-11-26 13:24:01 +0300

    Simple booking payment completed

--- handlers/my_books/booking_actions.py
+++ handlers/my_books/booking_actions.py
@@ -1,3 +1,4 @@
+from aiogram import types, Router, Bot
 from aiogram.fsm.context import FSMContext
 from lexicon.lexicon_ru import lexicon
 from keyboards.paginator.booking_paginator import create_booking_paginator_keyboard
@@ -6,24 +7,31 @@ from aiogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardBut
 import utils.db_api.db_commands as commands
 
 
-async def booking_pay(callback: CallbackQuery, state: FSMContext):
-    data = await state.get_data()
-
+async def booking_pay(callback: CallbackQuery):
     await callback.message.answer_invoice(
         title="Оплата парковки",
         description="Оплата брони парковочного места",
         provider_token='1744374395:TEST:7931d38eb15aa37e4b26',
         currency="rub",
         photo_url="https://cdn.iconscout.com/icon/premium/png-512-thumb/pay-parking-2607820-2181787.png?f=webp&w=256",
-        is_flexible=True,
+        is_flexible=False,
         photo_width=256,
         photo_height=256,
-        prices=[LabeledPrice(label='Парковка', amount=3000)],
-        payload="test-invoice-payload")
+        prices=[LabeledPrice(label='Парковка', amount=200 * 100)],
+        start_parameter="parking-booking",
+        payload="test-invoice-payload",
+        need_shipping_address=False)
 
-    await callback.message.answer(text='В меню', reply_markup=InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text='Назад', callback_data='my_booking')]]))
+
+# successful payment
+async def successful_payment(message: types.Message):
+    print("SUCCESSFUL PAYMENT:")
+    payment_info = message.successful_payment.to_python()
+    for k, v in payment_info.items():
+        print(f"{k} = {v}")
+
+    await message.answer(
+        f"Платеж на сумму {message.successful_payment.total_amount // 100} {message.successful_payment.currency} прошел успешно!!!")
 
 
 async def help_route(callback: CallbackQuery, state: FSMContext):
